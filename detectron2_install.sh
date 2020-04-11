44571848ff1cb40f333a54676783861940c1867d
Tuan Dang <37445602+danglive@users.noreply.github.com>
2020-04-11 19:16:19 +0200

    Add files via upload

---
+++ detectron2_install.sh
@@ -0,0 +1,4 @@
+pip install -U torch==1.4+cu100 torchvision==0.5+cu100 -f https://download.pytorch.org/whl/torch_stable.html 
+pip install cython pyyaml==5.1
+pip install git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI
+pip install git+https://github.com/facebookresearch/detectron2.git
